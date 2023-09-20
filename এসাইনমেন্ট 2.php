cd3b9652e3357bd3bfe7b31ad4ddc0ece22f2554
lorem200 <144935316+lorem200@users.noreply.github.com>
2023-09-20 21:20:49 +0600

    এসাইনমেন্ট 2.php

--- এসাইনমেন্ট 2.php
+++ এসাইনমেন্ট 2.php
@@ -1,12 +1,9 @@
 <?php
 for ($i = 1; $i <= 50; $i++) {
     // Check if $i is a multiple of 5
-    if ($i % 5 == 0) {
-        // Skip this iteration
+    if ($i % 5 == 0) {     
         continue;
-    }
-
-    // Print the number
+    }   
     echo $i . ' ';
 }
 
